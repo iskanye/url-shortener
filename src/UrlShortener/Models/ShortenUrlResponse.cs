6a9324580d2f6e71b3db89b6134f4bcc3fa59861
iskanye <iskanyeban@gmail.com>
2025-12-27 15:34:52 +0300

    feat: docker

--- src/UrlShortener/Models/ShortenUrlResponse.cs
+++ src/UrlShortener/Models/ShortenUrlResponse.cs
@@ -2,6 +2,6 @@ namespace UrlShortener.Models;
 
 public class ShortenUrlResponse(ShortUrl shortUrl, HttpContext context)
 {
-    public Guid If { get; set; } = shortUrl.Id;
+    public Guid Id { get; set; } = shortUrl.Id;
     public string ShortUrl { get; set; } = $"{context.Request.Scheme}://{context.Request.Host}/{shortUrl.Slug}";
 }
