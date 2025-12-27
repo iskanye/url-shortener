d7b0b9959c6514a6832d2bfb2a7106fed50aa224
iskanye <iskanyeban@gmail.com>
2025-12-27 17:19:20 +0300

    feat: postgresql

--- src/UrlShortener/DependencyInjection.cs
+++ src/UrlShortener/DependencyInjection.cs
@@ -5,7 +5,7 @@ public static class DependencyInjection
     public static void AddServices(this IHostApplicationBuilder builder)
     {
         builder.Services.AddDbContext<IApplicationContext, ApplicationContext>(o =>
-            o.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+            o.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
         builder.Services.AddScoped<IUrlShortenerServices, UrlShortenerService>();
     }
 }
